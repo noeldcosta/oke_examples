749b2fafb9f6f3f9d23e476f484d996227fc905c
noeldcosta <noel.dcosta@oracle.com>
2018-01-09 19:37:15 +0530

    Test 3

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello JD Edwards - test 2 !!"))
+	w.Write([]byte("Hello JD Edwards - test 3 !!"))
 }
 
 func main() {
