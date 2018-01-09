32bbfc60cffae2f35317d9679091bd322a94e65c
noeldcosta <noel.dcosta@oracle.com>
2018-01-09 15:54:56 +0530

    Second update

--- main.go
+++ main.go
@@ -8,11 +8,11 @@ import (
 )
 
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello JD Edwards!!"))
+	w.Write([]byte("Hello JD Edwards - test 2 !!"))
 }
 
 func main() {
-	fmt.Printf("Starting Hello JD Edwards application... \n")
+	fmt.Printf("Starting Hello JDE application... \n")
 	http.HandleFunc("/", HelloWorld)
 	http.ListenAndServe(":8080", handlers.LoggingHandler(os.Stdout, http.DefaultServeMux))
 }
